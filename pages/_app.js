e3572db749df8f844e8a507774ff516a8bbefee6
riho <rihoriho1031@gmail.com>
2020-08-19 11:13:26 +0900

    add gtag.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import App from "next/app";
+import Router from "next/router";
+import "../styles/globals.css";
+import * as gtag from "../lib/gtag";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
-export default MyApp
+export default App;
