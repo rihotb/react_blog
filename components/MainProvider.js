26cf9daf44eb28ed634ff666c325e5adadd3d6ed
riho <rihoriho1031@gmail.com>
2020-08-27 16:12:05 +0900

    initOffsetValue

--- components/MainProvider.js
+++ components/MainProvider.js
@@ -1,9 +1,37 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 export const MainContext = React.createContext();
 
+//propsで初期値を渡す
 const MainProvider = (props) => {
-  const [offsetValue, setOffsetValue] = useState(0);
+  const router = useRouter();
+  let initOffsetValue;
+
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    if (router.query) {
+      setPage(router.query.page);
+    }
+  }, [router]);
+
+  // pageが取得されてセットされたら処理される
+  useEffect(() => {
+    if (page) {
+      //ページ番号を元に、initOffsetValueの値を計算
+      initOffsetValue = (page - 1) * 10;
+      console.log(page);
+      console.log(initOffsetValue);
+    }
+  }, [page]);
+
+  // console.log(router.query.page);
+  // //router.query.pageはページ番号がはいるけど、最初はundefined。
+  // //initOfsetValueもNaNが入ってしまう
+  // initOffsetValue = (router.query.page - 1) * 10;
+  // console.log(initOffsetValue);
+
+  const [offsetValue, setOffsetValue] = useState(initOffsetValue);
   const [tag, setTag] = useState([]);
 
   return (
