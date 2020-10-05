05571b7e677b98140191b957680d4373fd56807b
riho <rihoriho1031@gmail.com>
2020-10-05 20:39:53 +0900

    adjust tag size

--- components/article-list/Tag.js
+++ components/article-list/Tag.js
@@ -5,7 +5,7 @@ export const TagButtonStyles = styled.a`
   margin-right: 10px;
   margin-bottom: 10px;
   display: inline-block;
-  padding: 0.3em 1em;
+  padding: 0.2em 0.5em;
   color: ${(props) => props.theme.colors.gray};
   border: solid 2px ${(props) => props.theme.colors.gray};
   border-radius: 5px;
